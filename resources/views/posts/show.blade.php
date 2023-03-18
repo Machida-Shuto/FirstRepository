5006135692a6fea7eb098a72e0550347ebb54418
Machida-Shuto <e200472@mail.ryukoku.ac.jp>
2023-03-18 19:07:33 +0900

    ブログ投稿の編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,9 +21,6 @@
             <div class='edit'>
                 <a href="/posts/{{ $post->id }}/edit">edit</a>
             </div>
-            <div class='edit'>
-                <a href="/posts/{{ $post->id }}/edit">edit</a>
-            </div>
             <div class='footer'>
                 <a href="/">戻る</a>
             </div>
